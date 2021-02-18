97242bbe6626b2c5edfa2ab39aa679c0748774db
Drugov Viktor <engistau@gmail.com>
2021-02-18 15:22:06 +0600

    .CreateAbout

---
+++ BizBox DLC/src/sample/controllers/AboutController.java
@@ -0,0 +1,98 @@
+package sample.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+
+public class AboutController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private MenuBar MainMenuBar;
+
+    @FXML
+    private Menu QeryMenuID;
+
+    @FXML
+    private MenuItem QeryMenuZeroingAmbulatoryId;
+
+    @FXML
+    private MenuItem QeryMenuChangePaymentId;
+
+    @FXML
+    private MenuItem QeryMenuChangeDoctorId;
+
+    @FXML
+    private MenuItem QeryMenuChangeAmbulatoryDateId;
+
+    @FXML
+    private MenuItem QeryMenuChangeBirthdateId;
+
+    @FXML
+    private MenuItem QeryMenuEnabledUltrasoundId;
+
+    @FXML
+    private MenuItem QeryMenuRecoveryUltrasoundId;
+
+    @FXML
+    private Menu CorpMenuId;
+
+    @FXML
+    private MenuItem CorpMenuAddCorpId;
+
+    @FXML
+    private MenuItem CorpMenuZeroingCorpId;
+
+    @FXML
+    private Menu DeleteMenuId;
+
+    @FXML
+    private MenuItem DeleteMenuDeletPaymentId;
+
+    @FXML
+    private MenuItem DeleteMenuDeletDiscountId;
+
+    @FXML
+    private MenuItem DeleteMenuDeletAmbulatoryId;
+
+    @FXML
+    private MenuItem DeleteMenuRecordReturnId;
+
+    @FXML
+    private Menu OptionsMenuId;
+
+    @FXML
+    private MenuItem OptionsMenuAccountId;
+
+    @FXML
+    private MenuItem OptionsMenuLogsId;
+
+    @FXML
+    private MenuItem OptionsMenuUsersId;
+
+    @FXML
+    private MenuItem OptionsMenuRebookId;
+
+    @FXML
+    private MenuItem OptionsMenuAboutId;
+
+    @FXML
+    private Label InfoQery;
+
+    @FXML
+    private Label InfoQery1;
+
+    @FXML
+    void initialize() {
+
+    }
+}
